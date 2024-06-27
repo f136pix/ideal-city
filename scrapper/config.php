b2f4ea2cbf2cf861d5455885ea8db65db7fe31e7
Filipe <filipecocinel@gmail.com>
2024-06-26 22:35:48 -0300

    fix

--- scrapper/config.php
+++ scrapper/config.php
@@ -1,6 +1,7 @@
 <?php
 
-define('RABBITMQ_HOST', 'localhost');
+define('HIGHEST_COST_OF_LIVING', 5134.8294545455);
+define('RABBITMQ_HOST', 'node-port');
 define('RABBITMQ_PORT', 5672);
 define('RABBITMQ_USER', 'guest');
 define('RABBITMQ_PASSWORD', 'guest');
